7ebd664a2416fb84b56e3c08519bca94e86f9bd2
Jamesart05 <130219176+Jamesart05@users.noreply.github.com>
2025-07-23 15:25:42 +0100

    fourth commit again

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Home from './pages/home';
+import Navbar from './components/navbar';
 
 function App() {
 
   return (
-<<<<<<< HEAD
-   <div>
-    <Navbar />
-   </div>
-=======
-    <>
-      <Home/>
-    </>
->>>>>>> 888c77d42b82d8f1241d82b64cabf420b4c48c59
+    <div>
+      <Navbar />
+      <Home />
+    </div>
   )
 }
 
